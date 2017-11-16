bbd3d64025b3996e12c6fe69d50c7cd25da2896d
Charles Ballew <charleswballewjr@gmail.com>
2017-11-16 10:50:41 -0700

    possible fix to eslint

--- 1-unit/part-1_websites/TS-2/galvanize-money/drills/version-2/index.js
+++ 1-unit/part-1_websites/TS-2/galvanize-money/drills/version-2/index.js
@@ -1,41 +1,41 @@
 // Your code here
 
 var register = {
-	transactions: []
+    transactions: []
 };
 
 function addTransaction(transaction) {
-	register.transactions.push(transaction);
+    register.transactions.push(transaction);
 }
 
 function reset() {
-	register.transactions = [];
+    register.transactions = [];
 }
 
 function countSalesOfType(itemType) {
-  var smallItemType = itemType.toLowerCase();
-  for (var i = 0; i < register.transactions.length; i++) {
-  	for (var j = 0; j < register.transactions[i].items.length; j++) {
-  		var itemDescription = register.transactions[i].items[j].description.toLowerCase();
-  		if (smallItemType === itemDescription) {
-  			var itemQuantity = register.transactions[i].items[j].quantity;
-  			return itemQuantity;
-  		}
-  	}
-  }
+    var smallItemType = itemType.toLowerCase();
+    for (var i = 0; i < register.transactions.length; i++) {
+        for (var j = 0; j < register.transactions[i].items.length; j++) {
+            var itemDescription = register.transactions[i].items[j].description.toLowerCase();
+            if (smallItemType === itemDescription) {
+                var itemQuantity = register.transactions[i].items[j].quantity;
+                return itemQuantity;
+            }
+        }
+    }
 }
 
 function getTransactionsBetween(transaction1, transaction2) {
-	var transactionsArray = [];
-	var transaction1 = new Date(transaction1);
-	var transaction2 = new Date(transaction2);
-  	for (var i = 0; i < register.transactions.length; i++) {
-  		var pastDates = register.transactions[i].date;
-  		if (pastDates >= transaction1 && pastDates <= transaction2) {
-  			transactionsArray.push(register.transactions[i]);
-  		}
-  	}
-	return transactionsArray;
+    var transactionsArray = [];
+    var transaction1 = new Date(transaction1);
+    var transaction2 = new Date(transaction2);
+    for (var i = 0; i < register.transactions.length; i++) {
+        var pastDates = register.transactions[i].date;
+        if (pastDates >= transaction1 && pastDates <= transaction2) {
+            transactionsArray.push(register.transactions[i]);
+        }
+    }
+    return transactionsArray;
 }
 module.exports = {
     // Uncomment these as you define them

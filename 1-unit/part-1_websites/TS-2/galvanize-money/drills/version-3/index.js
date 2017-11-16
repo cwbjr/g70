60c972ad1da33a036967d1e0daffc6e5e6f48f18
Charles Ballew <charleswballewjr@gmail.com>
2017-11-16 10:56:48 -0700

    fixed spaces in index

--- 1-unit/part-1_websites/TS-2/galvanize-money/drills/version-3/index.js
+++ 1-unit/part-1_websites/TS-2/galvanize-money/drills/version-3/index.js
@@ -1,30 +1,30 @@
 // Your code here
 
 var register = {
-  transactions: []
+    transactions: []
 };
 
 function addTransaction(transaction) {
-  register.transactions.push(transaction);
+    register.transactions.push(transaction);
 }
 
 function reset() {
-	register.transactions = [];
+    register.transactions = [];
 }
 
 function getTransactionsBySalesperson(cashierName) {
-	var salesTransactions = [];
-	for (let i = 0; i < register.transactions.length; i++) {
-		var getName = register.transactions[i].cashier;
-		if (cashierName === getName) {
-			salesTransactions.push(register.transactions[i]);
-		}
-	}
-	return salesTransactions;
+    var salesTransactions = [];
+    for (let i = 0; i < register.transactions.length; i++) {
+        var getName = register.transactions[i].cashier;
+        if (cashierName === getName) {
+            salesTransactions.push(register.transactions[i]);
+        }
+    }
+    return salesTransactions;
 }
 
 function removeLastTransaction() {
-	register.transactions.pop();
+    register.transactions.pop();
 }
 
 module.exports = {

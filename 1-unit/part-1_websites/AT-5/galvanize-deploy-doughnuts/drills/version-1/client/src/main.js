490714372a9e5309034f0cf65e4138aad47d86d3
Charles Ballew <charleswballewjr@gmail.com>
2017-11-22 17:27:20 -0700

    removed /cake

--- 1-unit/part-1_websites/AT-5/galvanize-deploy-doughnuts/drills/version-1/client/src/main.js
+++ 1-unit/part-1_websites/AT-5/galvanize-deploy-doughnuts/drills/version-1/client/src/main.js
@@ -3,7 +3,7 @@ let add = document.getElementById('append')
 
 button.addEventListener('click', (e) => {
   add.innerText = ''
-  fetch('https://at-5a-heroku.firebaseapp.com/cake')
+  fetch('https://at-5a-heroku.firebaseapp.com')
   .then((res) => res.json())
   .then((data) => {
     data.forEach((item) => {
